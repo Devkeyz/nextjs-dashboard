f83d80686467a220a542dbb79995c7479ccd73d3
Maksym Fedorchenko <deflyo@gmail.com>
2024-09-16 23:14:50 +0300

    Initial commit 2

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
+import React, { ReactNode } from 'react';
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-export default function Layout({ children }: { children: RecordingState.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-node md:w-64">
